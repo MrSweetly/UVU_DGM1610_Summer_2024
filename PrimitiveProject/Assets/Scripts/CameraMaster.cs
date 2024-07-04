c92b654a5570d7fba1e8c77c9ff0a34b2a9da525
Brandon Gomez <brandon.gomez.alex@gmail.com>
2024-07-04 03:12:27 -0600

    Added player/camera movement to PrimitiveProject

---
+++ PrimitiveProject/Assets/Scripts/CameraMaster.cs
@@ -0,0 +1,29 @@
+
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMaster : MonoBehaviour
+{
+   [SerializeField] private Transform target;
+   private Vector3 offset;
+   private Vector3 currentVelocity = Vector3.zero;
+   
+   private float distanceToPlayer;
+   private float smoothTime = 0.25f;
+
+   private void Awake()
+   {
+      // Offset Camera
+      offset = transform.position - target.position;
+   }
+
+   private void LateUpdate()
+   {
+      // Follow Camera
+      var targetPosition = target.position + offset;
+      transform.position = Vector3.SmoothDamp(transform.position, targetPosition,
+         ref currentVelocity, smoothTime);
+   }
+
+}
